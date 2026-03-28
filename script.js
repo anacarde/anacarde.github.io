c5a8df94c87e09e75ab7b3c1a0fcb09229973b00
Auguste Blanco <ookikoudai@outlook.fr>
2026-03-28 18:52:59 +0100

    $*

--- script.js
+++ script.js
@@ -3,7 +3,11 @@ document.addEventListener("DOMContentLoaded", function() {
 
   btn.addEventListener("click", function(e) {
     e.preventDefault();
-    navigator.clipboard.writeText("lazare.monin@gmail.com");
-    alert("Adresse copiée !");
+    navigator.clipboard.writeText("lazare.monin@gmail.com").then(() => {
+    copyMail.classList.add("copied");
+
+    setTimeout(() => {
+      copyMail.classList.remove("copied");
+    }, 1600);
   });
 });
